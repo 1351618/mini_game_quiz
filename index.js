addfd6b71b1061731a471de3b2c8e3eed33bd302
1351618 <1351618@gmail.com>
2023-10-26 04:28:16 +0300

    favicon

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// звук таймера
-const audio = document.getElementById("myAudio");
+// >>>>>>>>>>>>>>>>>>
 // таймер
 const timer = document.querySelector(".timer__p");
 const timerSubstrate = document.querySelector(".timer-substrate");
+// звук таймера
+const audio = document.getElementById("myAudio");
 
 const timerTime = 20; // таймер 20 сек
 
 function startTimer() {
-  console.log("запуск таймера");
+  //   console.log("запуск таймера");
   let i = timerTime;
   timer.style.textShadow =
     "0px 0px 2px #fff900, 0px 0px 6px #fff900, 0px 0px 15px #fff900";
@@ -21,7 +22,7 @@ function startTimer() {
     timer.textContent = `${i.toString().length < 2 ? `0${i}` : i}`;
     if (i === 0) {
       clearInterval(intervalId); // Остановка интервала при достижении 0
-      console.log("Таймер завершен");
+      //   console.log("Таймер завершен");
       setTimeout(() => {
         timer.textContent = `${timerTime}`;
         setProgress(0);
@@ -33,11 +34,13 @@ function startTimer() {
     }
   }, 1000);
 }
+
+// >>>>>>>>>>>>>>>>>>
 // таймер кольцо прогресса
 const circleTimer = document.querySelector(".progress-ring__circle");
 const radiusCircleTimer = circleTimer.r.baseVal.value;
 const circumference = 2 * Math.PI * radiusCircleTimer;
-console.log(circumference);
+// console.log(circumference);
 
 circleTimer.style.strokeDasharray = `${circumference} ${circumference}`;
 circleTimer.style.strokeDashoffset = circumference;
@@ -47,7 +50,7 @@ function setProgress(percent) {
   circleTimer.style.strokeDashoffset = offset;
 }
 
-// ========================================================
+// >>>>>>>>>>>>>>>>>>
 
 // соотношение 30 к 0% и 0 К 100%
 function procentBarr(i) {
@@ -58,9 +61,54 @@ function procentBarr(i) {
 // =======================================================
 // обработчик клавиатуры
 document.addEventListener("keydown", (event) => {
-  console.log(event.key);
+  //   console.log(event.key);
   if (event.key === " ") {
     console.log("space");
     startTimer();
   }
+});
+
+// =======================================================
+// todo настройка отображения страниц
+const main_Page = document.querySelector(".main-page");
+const round_1_Page = document.querySelector(".round-1");
+const round_2_Page = document.querySelector(".round-2");
+const round_3_Page = document.querySelector(".round-3");
+const question_Page = document.querySelector(".question");
+
+function pageSelection(page) {
+  showPage = page;
+  main_Page.classList.toggle("hide", showPage !== "main-page");
+  round_1_Page.classList.toggle("hide", showPage !== "round-1");
+  round_2_Page.classList.toggle("hide", showPage !== "round-2");
+  round_3_Page.classList.toggle("hide", showPage !== "round-3");
+  question_Page.classList.toggle("hide", showPage !== "question");
+}
+// pageSelection("main-page");
+pageSelection("question");
+
+// ======================================================
+// кнопка домой
+const btnHome = document.querySelector(".btn-home");
+
+btnHome.addEventListener("click", () => {
+  pageSelection("main-page");
+});
+
+// кнопка раундов
+const btnRounds = document.querySelectorAll(".main-page button");
+
+btnRounds.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.textContent);
+    if (button.textContent === "РАУНД 1") {
+      pageSelection("round-1");
+    }
+    if (button.textContent === "РАУНД 2") {
+      pageSelection("round-2");
+    }
+    if (button.textContent === "РАУНД 3") {
+      pageSelection("round-3");
+    }
+  });
 });
